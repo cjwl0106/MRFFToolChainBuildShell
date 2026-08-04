be720b2912b3c2bcaacb13a7deae106a22e7f108
weloy <banwenlong@hikvision.com.cn>
2026-08-04 09:35:24 +0800

    更新

--- do-compile/apple/bluray.sh
+++ do-compile/apple/bluray.sh
@@ -11,7 +11,7 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the language governing permissions and
+# See the License for the specific language governing permissions and
 # limitations under the License.
 #
 
@@ -22,6 +22,10 @@ cd "$THIS_DIR"
 
 # bluray 1.5.0 switched from autotools to meson build system
 # Use meson-compatible.sh for building
+
+# iOS/tvOS 不支持 DiskArbitration（HAVE_MOUNT=0），也不使用 BDJ（USING_BDJAVA_BUILD_JAR=0）
+# 这些宏定义通过 CFLAGS 传递给编译器，用于条件编译 patch 3 的代码
+export CFLAGS="$MR_INIT_CFLAGS -DUSING_BDJAVA_BUILD_JAR=0 -DHAVE_MOUNT=0"
 
 MESON_OTHER_FLAGS="-Denable_examples=false -Denable_tools=false -Denable_devtools=false -Denable_docs=false -Dbdj_jar=disabled -Dfontconfig=disabled -Dfreetype=disabled"
 
